2cd17e3260078b1da45be26295151696a126483f
Glory ann Conwi <gaconwi@googlemail.com>
2023-12-19 12:32:00 +0100

    Challenge: Backend basics greeting

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 import { createServer } from "node:http";
 
-// export const server = …
+export const server = createServer((request, response) => {
+    console.log(request.url)
+    response.end("Hello, Glory!")
+})
+
+export default server;
